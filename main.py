d2670c31585407e07abcccfe2f3d1ca4a1a07c10
JARijsdijk <j.a.rijsdijk@student.tudelft.nl>
2024-03-20 15:00:36 +0100

    add simple for

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def load_json_file(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
 
+def main():
+    file_path = "data/challenge_set.json"  # Replace 'your_file_path.json' with the actual file path
+    data = load_json_file(file_path)
+    print("Date:", data["date"])
+    print("Version:", data["version"])
+    print("Playlists:")
+    print(len(data["playlists"]))
+    count = 0
+    track_uri = "spotify:track:3W3KtDwAIg3mAruSpnfG3Q"
+    for playlist in data["playlists"]:
+        for track in playlist["tracks"]:
+            if track["track_uri"] == track_uri:
+                count += 1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
