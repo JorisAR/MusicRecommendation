ab150d98cbab10748c3634d96bf652c709cefdad
Nils <47635432+HUILIANGAA@users.noreply.github.com>
2024-03-29 15:29:11 +0100

    100k of 1 mil subset

--- load.py
+++ load.py
@@ -374,7 +374,7 @@ def split_combined_playlists(input_file, output_folder, output_1, output_2):
         print("Difference in playlist count is too high. Manually adjust to maintain balance.")
 
 createEvalSet = True
-dataslices_max = 10 # increase if you want more data
+dataslices_max = 100000 # increase if you want more data
 
 def main():
     if not os.path.exists("data/intersected.json"):
